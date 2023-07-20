bc094bdb0a7a28e50552b2a7cc920e79589d5bf2
Влад Билоконь <vlad2004bil@gmail.com>
2023-07-20 19:58:34 +0300

    First fix

---
+++ resources/js/script.js
@@ -0,0 +1,144 @@
+/*jslint browser: true*/
+/*global $, window, location */
+
+
+function handlePageLoaded() {
+    'use strict';
+    $(window).on("load", function () {
+        $('#status').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+        // setScrollBehavior();
+    });
+}
+
+function setScrollBehavior() {
+    'use strict';
+    $("html").niceScroll({
+        cursorcolor: "#f74d65",
+        scrollspeed: "100",
+        cursorborder: "1px solid #f74d65",
+        horizrailenabled: "false",
+        cursorborderradius: "0px"
+    });
+}
+
+function handleLinkClick() {
+    'use strict';
+    $('a[href*="#"]:not([href="#"])').on("click", function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+}
+
+
+// Used by includes to keep placeholder
+function unWrapPlaceholder() {
+    'use strict';
+    $(window).contents().unwrap();
+}
+
+// Closes responsive menu when a scroll trigger link is clicked
+$('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+function init() {
+    'use strict';
+    handlePageLoaded();
+    handleLinkClick();
+}
+
+
+function loc_url_eng() {
+    var p = window.location.pathname.split('/');
+    var page = p[p.length - 1].split('.');
+    var f = "../views_de/";
+    page[0] = f + page[0] + ".html";
+    
+    if (p==",index.html") {
+        f= '';
+        page[0]="index.de.html";
+    }
+     
+    location.href = page[0];
+
+}
+
+
+function loc_url_de() {
+    var p = window.location.pathname.split('/');
+    var page = p[p.length - 1].split('.');
+    var f = "../views/";
+    page[0] = f + page[0] + ".html";
+
+    if (p==",index.de.html") {
+        f= '';
+        page[0]="index.html";
+    }
+  
+    location.href =   page[0];
+
+}
+
+// var CntStorage = parseInt(sessionStorage.getItem("counter"));
+// if (isNaN(CntStorage)) {
+//     CntStorage=0;
+//     sessionStorage.setItem("counter", CntStorage);
+// }
+// sessionStorage.setItem('myLng', window.navigator.language);
+// var LngStorage = sessionStorage.getItem("myLng");
+
+// function load_page_de() {
+//     var k = window.location.pathname.split('/');
+//     var page = k[k.length - 1].split('.');
+//     var v = "../views/";
+    
+
+//     if (LngStorage != 'de' && CntStorage == 0 ) {
+//         if (k == ",index.de.html") {
+//             v = " ";
+//             location.href = v + page[0] + ".html";
+//         }
+//         location.href = v + page[0] + ".html";
+//     }
+
+//     CntStorage=1;
+//     sessionStorage.setItem("counter", CntStorage);
+// }
+
+// function load_page_eng() {
+//     var k = window.location.pathname.split('/');
+//     var page = k[k.length - 1].split('.');
+//     var v = "../views_de/";
+
+//      if (LngStorage == 'de' && CntStorage == 0) {
+//         if (k == ",index.html") {
+//             v = " ";
+//             location.href = v + page[0] + ".html";
+//         }
+//         location.href = v + page[0] + ".html";
+//     }
+    
+//     CntStorage=1;
+//     sessionStorage.setItem("counter", CntStorage);
+// }
+
+
+// let languageFistTwo = language.substr(0,2); // To only keep the first 2 characters.
+// let currentLocation = document.getElementsByTagName('html')[0].getAttribute('lang-js')
+
+
+
+
+init();
